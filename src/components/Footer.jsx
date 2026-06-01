55dea460b6e28c451618e2f13afd698d8103436e
hanif2862 <117036614+hanif2862@users.noreply.github.com>
2026-06-01 16:29:18 +0700

    Update footer with social media links
    Added links to GitHub and Telegram in the footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,28 @@ export default function Footer() {
         <span className="footer-tagline">Built on OPN Chain. Permissionless. Transparent. Decentralized.</span>
       </div>
       <div className="footer-right">
-        <img src="https://static.codia.ai/s/image_758cf4e0-879b-451c-8c41-dcce61d89e90.png" alt="github" className="footer-icon" />
+      <a href="https://github.com/hanif2862/Veltrix-Yield-Vault" 
+            target="_blank"
+            rel="noreferrer"
+        >
+        <img
+            src="https://static.codia.ai/s/image_758cf4e0-879b-451c-8c41-dcce61d89e90.png"
+            alt="github"
+            className="footer-icon"
+        />
+      </a>
         <img src="https://static.codia.ai/s/image_1b8d1dc5-44ff-4d00-8011-b121cb8bb246.png" alt="twitter" className="footer-icon" />
         <img src="https://static.codia.ai/s/image_041b40e2-d741-432e-8a35-0f6c1db2862c.png" alt="discord" className="footer-icon" />
-        <img src="https://static.codia.ai/s/image_1b8d1dc5-44ff-4d00-8011-b121cb8bb246.png" alt="telegram" className="footer-icon" />
+      <a href="https://t.me/fatthurrrr"
+          target="_blank"
+          rel="noreferrer"
+        >
+        <img
+            src="https://static.codia.ai/s/image_1b8d1dc5-44ff-4d00-8011-b121cb8bb246.png"
+            alt="telegram"
+            className="footer-icon"
+         />
+      </a>
         <div className="footer-divider-v" />
         <img src="https://static.codia.ai/s/image_dfd15fda-cf29-4b33-8e72-0188469d94ac.png" alt="docs" className="footer-icon-sm" />
         <span className="footer-docs">Docs</span>
